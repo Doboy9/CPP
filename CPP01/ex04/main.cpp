d24b6009a90901419d5ba756b28b5af758ce327a
Doboy <dorian.boire98@gmail.com>
2024-06-28 17:27:31 +0200

    Feat : Searching in the file the str

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,13 +6,34 @@
 /*   By: dboire <dboire@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 14:54:11 by dboire            #+#    #+#             */
-/*   Updated: 2024/06/19 14:57:59 by dboire           ###   ########.fr       */
+/*   Updated: 2024/06/28 17:27:03 by dboire           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Sed.hpp"
 
-int main (void)
+int main (int ac, char **av)
 {
-	
+	(void)av;
+	if(ac != 4)
+		std::cout << "Please enter a filename and two strings" << std::endl;
+	else
+	{
+		std::ifstream infile;
+		infile.open(av[1]);
+		if(!infile)
+		{
+			std::cerr << "Error opening the file" << std::endl;
+			return (1);
+		}
+		std::string line;
+		while(std::getline(infile, line))
+		{
+			if(line.find(av[2]) != std::string::npos)
+				std::cout << "found" << std::endl;
+			std::cout << line << std::endl;
+		}
+		infile.close();
+	}
+	return (0);
 }
