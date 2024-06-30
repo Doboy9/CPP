df33b59eac3d20e49f388a18b94a8204adf12e25
Doboy <dorian.boire98@gmail.com>
2024-06-30 10:51:47 +0200

    Feat : ex04 ok

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -14,30 +14,49 @@
 
 int main (int ac, char **av)
 {
-	(void)av;
+	std::size_t found;
 	std::string str;
+	std::string arg1 = av[2];
 	
 	if(ac != 4)
 		std::cout << "Please enter a filename and two strings" << std::endl;
 	else
 	{
 		std::ifstream infile;
+		
 		infile.open(av[1]);
 		if(!infile)
 		{
 			std::cerr << "Error opening the file" << std::endl;
 			return (1);
 		}
+		
 		std::string line;
+		
 		while(std::getline(infile, line))
 		{
 			str += line;
 			line = "";
 		}
-		if(str.find(av[2] != std::string::npos))
-			
-		std::cout << str << std::endl;
+		
+		found = str.find(av[2]);
+		
+		while (found != std::string::npos)
+		{
+			str.erase(found, arg1.length());
+			str.insert(found, av[3]);
+			found = str.find(av[2], found + arg1.length());
+		}
+		
 		infile.close();
+		
+		std::string replace = av[1];
+		std::ofstream outfile;
+		
+		replace += ".replace";
+		outfile.open(replace.c_str());
+		outfile << str;
+		outfile.close();
 	}
 	return (0);
 }
