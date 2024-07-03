7dbce796cf15448512ab49fb0921783e50e90efa
Doboy <dorian.boire98@gmail.com>
2024-07-03 14:04:23 +0200

    FIx : CPP01 pushing

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -14,7 +14,7 @@
 
 Zombie* zombieHorde( int N, std::string name )
 {
-	if(N < 0)
+	if(N <= 0)
 		return (NULL);
 	Zombie *Horde = new Zombie[N];
 	for (int i = 0; i < N; i++)
--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -16,6 +16,7 @@
 HumanB::HumanB(std::string name)
 {
 	_name = name;
+	_weapon = NULL;
 }
 
 HumanB::~HumanB(){};
@@ -28,8 +29,14 @@ void HumanB::setWeapon(Weapon &weapon)
 
 void HumanB::attack()
 {
-// VERIFY IF HUMANB HAS A WEAPON ELSE HE FIGHT WITH HIS HAND (FIST)
-	std::cout << _name
-	<< " attacks with their " 
-	<<  _weapon->getType() << std::endl;
+	std::cout << _name;
+	if(_weapon)
+	{
+		std::cout << " attacks with their "
+		<<  _weapon->getType() << std::endl;
+	}
+	else
+	{
+		std::cout << " attacks with their fists" << std::endl; 
+	}
 }
