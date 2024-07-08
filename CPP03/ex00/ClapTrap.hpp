697c00cfe388be01c289fb3fb08deeea8bb9ff13
Doboy <dorian.boire98@gmail.com>
2024-07-08 10:27:15 +0200

    CPP03

---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dboire <dboire@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 09:44:43 by dboire            #+#    #+#             */
+/*   Updated: 2024/07/08 09:51:50 by dboire           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+
+private:
+    std::string _name;
+    int         _hit_points;
+    int         _energy_points;
+    int         _attack_damage;
+
+
+public:
+    ClapTrap();
+    ~ClapTrap();
+
+    void    attack(const std::string& target);
+    void    takeDamage(unsigned int amount);
+    void    beRepaired(unsigned int amount);
+};
+
+#endif
